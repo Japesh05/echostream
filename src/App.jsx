db3a626916a65f2983412eafe953ce6cc055641d
Japesh <goyaljapesh05@gmail.com>
2024-08-17 17:54:41 +0530

    Ready to deploy

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+import { Routes, Route, Navigate } from "react-router-dom"
+import Home from "./Home/Home";
+import Post from "./Post/Post";
+import { useSelector } from "react-redux"
+import io from "socket.io-client";
+import { createContext } from "react";
+import Bookmark from "./Bookmark/Bookmark";
+import Lost from "./Error404/Lost";
+
+const ioContext = createContext();
+
+// sup db pass
+// W7AtlJxYZO1uIxQJ
+
+function App() {
+
+  // const isLoggedin = useSelector(state => state.isLoggedin) | (localStorage.getItem("isLoggedin") === 'true');
+  const isLoggedin = useSelector(state => state.authReducer.isLoggedin);
+  const id = useSelector(state => state.authReducer.id);
+  const socket = io.connect(process.env.REACT_APP_SOCKET_URL || "http://localhost:3001");
+
+  return (
+    <ioContext.Provider value={{socket}}>
+    <Routes>
+      <Route exact path="/" element = { isLoggedin ? <Navigate to= {`/home/${id}`} /> : <Navigate to= "/auth/login" />}></Route>
+      <Route exact path="/auth/login" element = { isLoggedin ? <Navigate to= {`/home/${id}`} /> : <Login/> }></Route>
+      <Route exact path="/auth/signup" element = { isLoggedin ? <Navigate to= {`/home/${id}`} /> : <Signup/> }></Route>
+      <Route exact path="/home/:id" element = { isLoggedin ? <Home/> : <Navigate to= "/auth/login" /> }></Route>
+      <Route exact path="/bookmark/:id" element = { isLoggedin ? <Bookmark/> : <Navigate to= "/auth/login" /> }></Route>
+      <Route path="*" element = {<Lost />}></Route>
+      <Route exact path="/post" element = { isLoggedin ? <Post/> : <Navigate to= "/auth/login" /> }></Route>
+    </Routes>
+    </ioContext.Provider>
+  );
+}
+
+export default App;
+export { ioContext };
